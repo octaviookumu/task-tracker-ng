6e5a5894fac43a22077ce4e0ecee37248ecdd29f
Octavian Okumu <octaviookumu@gmail.com>
2022-01-20 16:23:50 +0300

    Doubleclick to change reminder colour

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -5,9 +5,17 @@ import { Task } from '../Task';
 import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+  }),
+};
+
 @Injectable({
   providedIn: 'root',
 })
+  
+  
 export class TaskService {
   private apiUrl = 'http://localhost:5000/tasks';
 
@@ -25,5 +33,10 @@ export class TaskService {
   deleteTask(task: Task): Observable<Task> {
     const url = `${this.apiUrl}/${task.id}`;
     return this.http.delete<Task>(url);
+  }
+
+  updateTaskReminder(task: Task): Observable<Task> {
+    const url = `${this.apiUrl}/${task.id}`;
+    return this.http.put<Task>(url, task, httpOptions)
   }
 }
